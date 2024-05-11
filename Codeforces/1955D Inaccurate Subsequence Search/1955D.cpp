785e6dc97532f0aa66f77597050c875372c9c6a7
ntphuong120 <nguyenphuong11209@gmail.com>
2024-05-11 08:57:11 +0700

    technique sliding window - 1955D

--- Codeforces/1955D Inaccurate Subsequence Search/1955D.cpp
+++ Codeforces/1955D Inaccurate Subsequence Search/1955D.cpp
@@ -1,69 +1,44 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
- 
-void ReadInput(int &n, int &m, int &k, vector<int> &a, vector<int> &b) {
+
+#define FAST_IO (ios_base::sync_with_stdio(false), cin.tie(NULL));
+#define endl '\n'
+#define ff first
+
+void solve()
+{
+    int n, m, k;
     cin >> n >> m >> k;
- 
-    a.resize(n + 1);
-    for(int i = 1; i <= n; ++i)
-        cin >> a[i];
-    
-    b.resize(m + 1);
-    for(int i = 1; i <= m; ++i)
-        cin >> b[i];
-}
- 
-bool timkiem(vector<int> b, const int &m, const int &x) {
-    int l = 1, r = m;
- 
-    while(l <= r) {
-        int mid = (l + r)/2;
-        
-        if(b[mid] == x) return true;
-        else if(b[mid] > x)
-            r = mid - 1;
-        else 
-            l = mid + 1;
+    vector<int> a(n), b(m);
+    for (auto &i : a)
+        cin >> i;
+    for (auto &i : b)
+        cin >> i;
+    map<int, int> cta, ctb;
+    for (auto x : b)
+        ctb[x]++;
+    for (int i = 0; i < m; ++i)
+        cta[a[i]]++;
+    int ans = 0, ct = 0;
+    for (auto e : ctb)
+        ct += min(cta[e.ff], ctb[e.ff]);
+    ans += (ct >= k);
+    for (int i = m; i < n; ++i)
+    {
+        cta[a[i - m]]--;
+        ct -= (ctb[a[i - m]] > cta[a[i - m]]);
+        cta[a[i]]++;
+        ct += (ctb[a[i]] >= cta[a[i]]);
+        ans += (ct >= k);
     }
-    
-    return false;
+    cout << ans << endl;
 }
- 
-const int maxn = 1e6 + 1;
-void solve(const int &n, const int &m, const int &k, vector<int> &a, vector<int> &b) {
-    sort(b.begin(), b.end());
- 
-    int result = 0, count = 0;
-    bool used[maxn];
-    for(int i = 1; i <= n - m + 1; ++i) {
-        memset(used, false, sizeof(used));
-        for(int j = i; j < i + m; ++j) {
-            if(!used[a[j]] && timkiem(b, m, a[j])) {
-                count++;
-            }
-            if(count >= k) {
-                result++;
-                break;
-            }
- 
-            used[a[j]] = true;
-        }
-        count = 0;
-    }
- 
-    cout << result << '\n';
-}
- 
-int main() {
-    freopen("Phuong.inp", "r", stdin);
- 
-    int t; 
-    cin >> t;
-    while(t--) {
-        int n, m, k;
-        vector<int> a, b;
-        ReadInput(n, m, k, a, b);
-        solve(n, m, k, a, b);
-    }
+
+int main()
+{
+    FAST_IO;
+    int TC = 1;
+    cin >> TC;
+    while (TC--)
+        solve();
 }
